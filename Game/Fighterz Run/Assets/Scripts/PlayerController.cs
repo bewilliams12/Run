15030dfa8b4204cf690ee36f4b13dda891e45a07
bewilliams12 <brebrew123@gmail.com>
2018-05-08 08:10:12 -0400

    Didn't have enough time to commit my work yesterday

--- Game/Fighterz Run/Assets/Scripts/PlayerController.cs
+++ Game/Fighterz Run/Assets/Scripts/PlayerController.cs
@@ -9,13 +9,41 @@ public class PlayerController : MonoBehaviour {
     public float jumpForce;
 
     private Rigidbody2D myRigidbody;
+
+    public bool grounded;
+    public LayerMask whatIsground;
+
+    private Collider2D myCollider;
+
+    private Animator myAnimatior;
+
+
 	// Use this for initialization
 	void Start () {
         myRigidbody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
+        myAnimatior = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        grounded = Physics2D.IsTouchingLayers(myCollider, whatIsground);
+
+        
+
+        myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space)||Input.GetMouseButtonDown(0))
+        {
+            //remove if(grounded){} but keep the code in statement to have infinte jumps
+            if (grounded)
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+            }
+
+        }
+        myAnimatior.SetFloat("Speed", myRigidbody.velocity.x);
+        myAnimatior.SetBool("Grounded", grounded);
 	}
 }
