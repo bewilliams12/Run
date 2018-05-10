9dbfccd554e215e252af42cccd87175c815121d3
bewilliams12 <brebrew123@gmail.com>
2018-05-10 09:58:08 -0400

    Added platform generation and destroyer

--- Game/Fighterz Run/Assets/Scripts/PlatGenerate.cs
+++ Game/Fighterz Run/Assets/Scripts/PlatGenerate.cs
@@ -7,14 +7,22 @@ public class PlatGenerate : MonoBehaviour {
     public Transform generationPoint;
     public float distanceBetween;
     private float platformWidth;
-
-	// Use this for initialization
-	void Start () {
+    public float distanceBetweenMin;
+    public float distanceBetweenMax;
+    // Use this for initialization
+    void Start () {
         platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (transform.position.x < generationPoint.position.x)
+        {
+            distanceBetween = Random.Range(distanceBetweenMin, distanceBetweenMax);
+
+            transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
+            
+            //Instantiate(thePlatform, transform.position, transform.rotation);
+        }
 	}
 }
---
+++ Game/Fighterz Run/Assets/Scripts/PlatformDestroyer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformDestroyer : MonoBehaviour {
+    public GameObject platDestructionPoint;
+
+
+
+	// Use this for initialization
+	void Start () {
+        platDestructionPoint = GameObject.Find("PlatDestructionPoint");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x < platDestructionPoint.transform.position.x)
+        {
+            //Destroy(gameObject);
+            gameObject.SetActive(false);
+        }
+	}
+}
